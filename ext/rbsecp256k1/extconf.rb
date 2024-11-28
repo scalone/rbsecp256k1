0795aa2eaa7a087f3320fc85315360564d7ebb1d
Thiago Scalone <thiago@scalone.com.br>
2024-11-28 14:29:18 -0300

    Update extconf.rb

--- ext/rbsecp256k1/extconf.rb
+++ ext/rbsecp256k1/extconf.rb
@@ -54,7 +54,7 @@ class Secp256k1Recipe < MiniPortile
   def extract_zip_file(file, destination)
     FileUtils.mkdir_p(destination)
 
-    Zip::File.open(file) do |zip_file|
+    ::Zip::File.open(file) do |zip_file|
       zip_file.each do |f|
         fpath = File.join(destination, f.name)
         zip_file.extract(f, fpath) unless File.exist?(fpath)
